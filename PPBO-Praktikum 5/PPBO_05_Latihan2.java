b35b7cf1ed9b04afa8061db345b511db67b5e5b0
adelinefellita <91838700+adelinefellita@users.noreply.github.com>
2022-11-10 14:45:04 +0700

    Add files via upload

---
+++ PPBO-Praktikum 5/PPBO_05_Latihan2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PPBO_05_Latihan2{
+    public static void main (String[] args){
+        String s;
+        Scanner input = new Scanner(System.in);
+        s = input.nextLine();
+        input.close();
+
+        int regex;
+        if(s.matches("^[aAiIuUeEoO].*")){
+            regex=1;
+        }
+        else if(s.matches("^(.*[^ng])(ng)")){
+            regex=2;
+        }
+        else{
+            regex=-1;
+        }
+        System.out.println(regex);
+    }
+}
